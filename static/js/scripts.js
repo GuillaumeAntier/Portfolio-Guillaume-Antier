b84b0773207817f62c04377b3cdd721299d88597
aguillau <guillaume.antier@ynov.com>
2024-11-08 16:01:17 +0100

    manque a faire les dégradé qui change et l'ajout du texte et des projets pour le contenu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,25 +4,41 @@ document.addEventListener('DOMContentLoaded', function() {
     const projectSections = document.querySelectorAll('.projet-content');
     const frameContentLeft = document.querySelector('.frame-content-left');
 
+    let currentProject = null;
+
     projectLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
 
-            projectSections.forEach(section => section.style.display = 'none');
-            welcomeSection.style.display = 'none';
-
             const projectId = this.getAttribute('data-id');
-            const projectContent = document.getElementById(projectId);
 
-            if (projectContent) {
+            if (currentProject === projectId) {
                 frameContentLeft.classList.add('fade-out');
 
                 setTimeout(() => {
-                    projectContent.style.display = 'block';
+                    projectSections.forEach(section => section.style.display = 'none');
+                    welcomeSection.style.display = 'block';
                     frameContentLeft.classList.remove('fade-out');
                 }, 500);
+
+                currentProject = null;
             } else {
-                welcomeSection.style.display = 'block';
+                frameContentLeft.classList.add('fade-out');
+
+                setTimeout(() => {
+                    projectSections.forEach(section => section.style.display = 'none');
+                    welcomeSection.style.display = 'none';
+
+                    const projectContent = document.getElementById(projectId);
+
+                    if (projectContent) {
+                        projectContent.style.display = 'block';
+                    }
+
+                    frameContentLeft.classList.remove('fade-out');
+                }, 500);
+
+                currentProject = projectId;
             }
         });
     });
