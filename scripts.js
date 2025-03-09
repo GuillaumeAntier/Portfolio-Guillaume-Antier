e3abc07d27626a053acf2464ac9ed6196e4d887f
aguillau <guillaume.antier@ynov.com>
2025-03-09 10:52:31 +0100

    changement du site

--- scripts.js
+++ scripts.js
@@ -42,31 +42,35 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-});
 
-// Get the modal
-var modal = document.getElementById("video-modal");
+    // Get the modal
+    var modal = document.getElementById("video-modal");
 
-// Get the link that opens the modal
-var link = document.getElementById("video-link");
+    // Get the link that opens the modal
+    var link = document.getElementById("video-link");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the link, open the modal
-link.onclick = function(event) {
-    event.preventDefault();
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // When the user clicks the link, open the modal
+    if (link) {
+        link.onclick = function(event) {
+            event.preventDefault();
+            modal.style.display = "block";
+        }
     }
-}
+
+    // When the user clicks on <span> (x), close the modal
+    if (span) {
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
